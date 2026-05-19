04ff633609d237ae6ce478f5a1fe640f0311ba55
Yash prakash <yashprakash166@gmail.com>
2026-05-20 01:33:34 +0530

    Integratecloudinary for permanent image uploads

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -1,21 +1,24 @@
 import express from "express";
-import upload from "../config/multer.js";
-import protect from "../middleware/authMiddleware.js";
-import admin from "../middleware/adminMiddleware.js";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
 const router = express.Router();
 
-// Upload single image file
-router.post(
-  "/",
-  protect,
-  admin,
-  upload.single("image"),
-  (req, res) => {
-    res.json({
-      image: `/uploads/${req.file.filename}`,
-    });
-  }
-);
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "ecommerce-products",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+  },
+});
+
+const upload = multer({ storage });
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send({
+    image: req.file.path,
+  });
+});
 
 export default router;
